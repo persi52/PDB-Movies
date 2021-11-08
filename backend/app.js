07aa56954d9bdfbca69726297f6cacf2a4e440c2
Daniel Zazuwiak <daniel.zazuwiak@wp.pl>
2021-11-08 23:10:42 +0100

    na dzis koniec tych navbarow

--- backend/app.js
+++ backend/app.js
@@ -29,7 +29,6 @@ app.use(function(req, res, next) {  //cross origin resource sharing, pozwolenie 
 app.use(flash());
 
 //Import Routes
-<<<<<<< HEAD
 const authRoute = require('./routes/auth-route');
 const streamRoute = require('./routes/stream-route');
 const movieRoute = require('./routes/movie-route');
@@ -44,17 +43,6 @@ app.use('/api/recommend/', recommendationRoute);
 app.use(express.urlencoded({ extended: true }))
 
 
-=======
-const authRoute = require('./routes/auth');
-const videoRoute = require('./routes/streaming');
-
-//Route Middlewares
-app.use('/api/users/', authRoute );
-app.use('/api/movies/', videoRoute);
-
-app.use(express.urlencoded({ extended: true }))
-
->>>>>>> master
 
 //PORT
 const port = process.env.PORT || 5000;
