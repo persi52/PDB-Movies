fd181595f5704cceda2bfcca8e0dd4508643fd61
persi52 <b.zima@alemoto.pl>
2021-11-29 18:19:12 +0100

    recommendation notifications

--- backend/controllers/recommendationController.js
+++ backend/controllers/recommendationController.js
@@ -3,21 +3,39 @@ const fs = require('fs');
 const router = express.Router();
 const pool = require('../models/db');
 const verifyToken = require("../controllers/verifyToken");
+const {sendNotification} = require("../controllers/notificationsController.js")
 
 const recommendMovie = async(req,res) =>{
-
-   // console.log(req.user.user_id)//res.send(req.param['set-cookie']);
+  
     const user = req.user;
-    console.log(user);
-    if(!user) return res.status(401).send('Access Denied'); 
+    
+    if(user.user_id==req.body.receiver_id) res.status(400).send('Wrong request');
 
     try{
-        pool.query('INSERT INTO recommendations (sender_id,receiver_id,movie_id)' +
-        'values ($1, $2, $3)',[req.user.user_id, req.body.receiver_id, req.body.movie_id],(err,results)=>{ 
-                   
-            res.status(200).send(results.rows);
-            //console.log(results);
+        pool.query('SELECT * FROM recommendations WHERE sender_id = $1 AND receiver_id = $2 ' +
+        'AND movie_id = $3',[user.user_id,req.body.receiver_id,req.body.movie_id],(err,results)=>{
+            
+            if(err) throw err;
+
+            if(results.rowCount>0) res.status(400).send('Movie was already recommended');
+            else{
+                pool.query('INSERT INTO recommendations (sender_id,receiver_id,movie_id)' +
+                'values ($1, $2, $3)',[user.user_id, req.body.receiver_id, req.body.movie_id],(err,results)=>{ 
+                           
+                    if(err) throw err;
+                    else sendNotification({
+                        sender_id : user.user_id,
+                        type : 'recommendation',
+                        movie_id : req.body.movie_id,
+                        receiver_id : req.body.receiver_id
+                    }).then(res.status(200).send('Movie recommended successfully to user ' + user.nickname));
+                    
+                })
+            }
+
+    
         })
+       
     }catch(err){
         res.status(403).send('Invalid statement');
         
@@ -28,3 +46,4 @@ const recommendMovie = async(req,res) =>{
 module.exports = {
     recommendMovie
 }
+
