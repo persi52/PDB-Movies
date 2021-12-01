c05770e59f4dbcaf716408001198c03e91314c44
Bartlomiej Zima <bartlomiej.zima@o2.pl>
2021-12-01 17:41:11 +0100

    getNotifications adjusting

--- backend/controllers/notificationsController.js
+++ backend/controllers/notificationsController.js
@@ -6,21 +6,79 @@ const pool = require('../models/db');
 
 
 const getUserNotifications = async(req,res) =>{
-  //const user = req.user;
+  const user_id = req.user.user_id;  
 
   try{
-    pool.query('SELECT * FROM notifications WHERE receiver_id=$1 ORDER BY notification_id DESC',[1],
+    pool.query('SELECT * FROM notifications WHERE receiver_id=$1 ORDER BY notification_id DESC',[user_id],
     (err,results)=>{
-        if(results.rows.length>0)
-        res.status(200).send(results.rows);
+      
+        if(results.rows.length>0){   
+                
+         sortNotifications(results.rows).then(data => res.status(200).send(data));       
+         
+        }
         else res.status(200).send('No nofifications');
+
+       
        // console.log(results);
     })
+    
     }catch(err){
     console.log(err);
     }
-       
+      
 }
+
+async function sortNotifications(notifications){ 
+
+  let sortedNotifications = [];
+  
+  for(const notification of notifications){
+
+    switch(notification.type){
+
+      case 'recommendation' : 
+      await pool.query('SELECT m.title, m.thumbnail, u.nickname, u.profile_picture ' + 
+      'FROM movies m, users u ' + 
+      'WHERE (movie_id=$1 AND user_id=$2)',[notification.movie_id,notification.sender_id])
+      .then( (data) =>{   
+
+        return {
+        type : 'recommendation',
+        movie_id : notification.movie_id,
+        movie_title : data.rows[0].title, 
+        sender_id : notification.sender_id,
+        sender_nickname : data.rows[0].nickname,
+        sender_profile_picture : data.rows[0].profile_picture              
+      }
+    })
+     .then((data) => {sortedNotifications.push(data)});     
+    
+        break;
+
+      case 'friendRequest' : 
+      await pool.query('SELECT nickname, profile_picture FROM users WHERE user_id=$1',[notification.sender_id])
+      .then( (data) => {  
+ 
+      return {
+       type : 'friendRequest',
+       sender_id : notification.sender_id,
+       nickname : data.rows[0].nickname,
+       sender_profile_picture : data.rows[0].profile_picture                
+                       
+      }
+    })
+    .then((data) => {sortedNotifications.push(data)});
+
+        break;
+    }
+    
+  }
+  return sortedNotifications;   
+    
+  
+}
+
 
 const sendNotification = async(req,res) =>{
   //const user = req.user;
