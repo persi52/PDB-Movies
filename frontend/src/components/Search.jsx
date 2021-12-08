b15f6dd097980be6be5eeb68f8ba7f723dc3104f
Bartlomiej Zima <bartlomiej.zima@o2.pl>
2021-12-08 14:25:46 +0100

    search functionality

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import '../css/reset.css'
 import '../css/style.css'
 import '../css/no_access.css'
-import { getMovies } from '../routes/movieRoutes';
+import { getMovies, getGenres } from '../routes/movieRoutes';
 import { useState, useEffect } from 'react';
 import {Link} from 'react-router-dom'
 import { getUsers } from '../routes/userRoutes';
@@ -13,30 +13,48 @@ export function Search() {
     const url = "movie/";
 
     useEffect(() =>{
-        getMovies().then((resp)=>{setMovies(resp)}); 
-        getUsers().then((resp)=>{setUsers(resp)});      
+        getMovies().then((resp)=>{setMovies(resp); console.log(resp) }); 
+        getUsers().then((resp)=>{setUsers(resp); console.log(resp)}); 
+        getGenres().then((resp) =>{setGenres(resp); console.log(resp)})
       }, []);
     
       const [movies, setMovies] = useState([]);
       const [users, setUsers] = useState([]);
+      const [genres, setGenres] = useState([]);
       const [searchTerm, setSearchTerm] = useState('');
 
   return (
     
     <div className="container">        
         <input type="text" placeholder="Szukaj..." onChange={event => {setSearchTerm(event.target.value)}}/>
-      
+        <h1 style={{"font-size": "xx-large"}}>Filmy</h1>
         {movies.filter((val) => {
             if(searchTerm == ""){
                 return val
-            } else if(val.title.toLowerCase().includes(searchTerm.toLowerCase())){
+            } else if(val.title.toLowerCase().includes(searchTerm.toLowerCase())
+            || genres.filter((obj) => {
+                
+                if(val.genre_id.find(element => element==obj.genre_id))
+                {
+                    //console.log(obj.name)
+                    return obj;
+                }
+            }).some(x => x.name.toLowerCase().includes(searchTerm.toLowerCase())))           
+            
+            {               
+            
+                  ///zeby sie przefiltrowaly to to musi w jakis sposob zwrocic true
+                
                 return val
             }
+            
+            
         }).map((val,key) => {
             return <p>{val.title}</p>
         })}
-
-        {/* {users.filter((val) => {
+        <br></br>
+        <h1 style={{"font-size": "xx-large"}}>Użytkownicy</h1>
+        {users.filter((val) => {
             if(searchTerm == ""){
                 return val
             } else if(val.nickname.toLowerCase().includes(searchTerm.toLowerCase())){
@@ -44,7 +62,7 @@ export function Search() {
             }
         }).map((val,key) => {
             return <p>{val.nickname}</p>
-        })} */}
+        })}
         
     </div>
             
